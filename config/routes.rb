278ae99c4e4fcc7e8b3befdbb1d0767e1892a215
sugiyamaF <104726857+sugiyamaF@users.noreply.github.com>
2022-10-10 05:42:37 +0000

    start

--- config/routes.rb
+++ config/routes.rb
@@ -1,71 +1,31 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'order_details/update'
-  end
-  namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
-    get 'homes/top'
+    resources :order_details
+    resources :orders
+    resources :customers
+    resources :genres
+    resources :items
+    resources :homes
   end
   namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
+    resources :addresses
+
+    resources :orders
     get 'orders/comfirm'
     get 'orders/complete'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
+
+    resources :cart_items
     get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
+
+    resources :customers
     get 'customers/unsubscribe'
     get 'customers/withdraw'
+
+    resources :homes
+    resources :items
   end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
+  
+
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
